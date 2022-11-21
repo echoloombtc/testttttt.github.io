3d5d02ecca28d900667d38622f4086af8f88ff1b
llowq <96781765+kenDevXD@users.noreply.github.com>
2022-11-21 19:21:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Kenn Hiroyuki',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792450877-40ef17b83bbb435b68d7c1c2',
+    'm792450943-b58f72f11ac64320184edcef',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://aixxy.me/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/kenDevXD'
+    },
+    {
+      text: 'Blog',
+      url: 'https://aixxy.me'
+    },
+  ],
+};
